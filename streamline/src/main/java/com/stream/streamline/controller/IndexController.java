aa430c2a6aacf4f33f63ba417eff86442e2b6150
Arman <arman_a@hcl.com>
2023-12-11 10:57:37 +0530

    Setting Up project by Arman Malik

---
+++ streamline/src/main/java/com/stream/streamline/controller/IndexController.java
@@ -0,0 +1,30 @@
+package com.stream.streamline.controller;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+
+//	@PostMapping("/contactus")
+//	public String registeration(@ModelAttribute ContactUs contactUs)
+//	{
+//		String name = contactUs.getName();
+//		String email = contactUs.getEmail();
+//		String subject = contactUs.getSubject();
+//		String message = contactUs.getMessage();
+//		System.out.println(contactUs);
+//		System.out.println(name + " "+ email);
+//		
+//		return "success";
+//	}
+}
