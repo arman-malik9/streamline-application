1375d0ad92ddf6011ec6a7e3cf213f9bdb45fe48
Ritik Bansal <ritik.bansal@hcl.com>
2023-12-20 18:09:18 +0530

    Added signup user model and integrated with controller

--- streamline/src/main/java/com/stream/streamline/controller/IndexController.java
+++ streamline/src/main/java/com/stream/streamline/controller/IndexController.java
@@ -1,4 +1,6 @@
 package com.stream.streamline.controller;
+import java.util.Date;
+
 import javax.security.auth.login.LoginException;
 import javax.security.auth.spi.LoginModule;
 
@@ -7,7 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.stream.streamline.model.LogInModel;
+import com.stream.streamline.model.SignUpModel;
 
 
 
@@ -16,12 +21,29 @@ import com.stream.streamline.model.LogInModel;
 public class IndexController {
 	
 	@GetMapping("/")
-	public String logIn() {
+	public String homepage() {
+		return "login";
+	}
+	
+	@RequestMapping("/signUp")
+	public String signUp(){
+		return "signUp";
+	}
+	
+	@PostMapping("/logIn")
+	public String registeration(@ModelAttribute SignUpModel signmodel) {
+		String name = signmodel.getName();
+		String DOB = signmodel.getDOB();
+		String email = signmodel.getEmail();
+		String password = signmodel.getPassword();
+		
+		System.out.println(signmodel);
+		
 		return "logIn";
 	}
 
 	@PostMapping("/home")
-	public String registeration(@ModelAttribute LogInModel loginmodel )
+	public String logIn(@ModelAttribute LogInModel loginmodel )
 	{
 		
 		String email = loginmodel.getEmail();
---
+++ streamline/src/main/java/com/stream/streamline/model/SignUpModel.java
@@ -0,0 +1,39 @@
+package com.stream.streamline.model;
+
+import java.util.Date;
+
+public class SignUpModel {
+	private String name;
+	private String DOB;
+	private String email;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDOB() {
+		return DOB;
+	}
+	public void setDOB(String DOB) {
+		this.DOB = DOB;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "SignUpModel [name="+ name + ", DOB="+ DOB + ", email=" + email + ", password=" + password + "]";
+	}
+}
